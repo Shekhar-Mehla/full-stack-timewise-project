feb386bd351361c96405f38ec4b01c2ed6863117
Shekhar Singh <smehla147@gmail.com>
2025-04-25 18:02:17 +1000

    added connection string

--- react_backend/src/Configuration/connection.js
+++ react_backend/src/Configuration/connection.js
@@ -2,9 +2,7 @@ import mongoose from "mongoose";
 export const mongodbConnection = async () => {
   try {
     console.log(process.env.MONGO);
-    const coonection = await mongoose.connect(
-      "mongodb+srv://Shekhar:M8hEhoPSugRWsdaG@cluster0.7ohfy.mongodb.net/time_wise?retryWrites=true&w=majority&appName=Cluster0"
-    );
+    const coonection = await mongoose.connect(process.env.MONGO);
     return coonection;
   } catch (error) {
     console.log(error.message);
