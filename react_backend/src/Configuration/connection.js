e1f6a7953f22b0f99ae2af74ca1dd77e70384210
Shekhar Singh <smehla147@gmail.com>
2025-04-25 18:38:31 +1000

    Revert "added .env"
    This reverts commit dcca19ec6b752ee263e3904a5c300fb9638e9702.

--- react_backend/src/Configuration/connection.js
+++ react_backend/src/Configuration/connection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 export const mongodbConnection = async () => {
   try {
     console.log(process.env.MONGO);
-    const coonection = await mongoose.connect(process.env.MONGO);
+    
     return coonection;
   } catch (error) {
     console.log(error.message);
